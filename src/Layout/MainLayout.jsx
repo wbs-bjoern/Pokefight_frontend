a0d3b2312af4449f531a0d12f1d90f807f70e92a
unknown <you@example.com>
2023-11-28 17:10:13 +0100

    routing, helmet, mainlayout

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+// import Header from "./Header/Index";
+// import Footer from "./Footer";
+
+export default function MainLayout({ children }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
